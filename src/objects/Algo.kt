93d29ec6d403b79eb77a6d6cb462b314f53f5407
jgr1585 <jgr1585@students.fhv.at>
2024-06-07 11:07:54 +0200

    Removed Unused

--- src/objects/Algo.kt
+++
@@ -1,9 +0,0 @@
-package objects
-
-enum class Algo {
-    ITERATIVE_DEEP_SEARCH,
-    A_STAR_SEARCH_H1,
-    A_STAR_SEARCH_H2,
-    GREEDY_SEARCH_H1,
-    GREEDY_SEARCH_H2,
-}
--- src/objects/AlgoResults.kt
+++
@@ -1,7 +0,0 @@
-package objects
-
-data class AlgoResults(
-    val algo: Algo,
-    val depth: Int,
-    val stats: AlgoStats
-)
